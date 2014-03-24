3d6121bdc517882ae947dbbcbbba67e6b621eae7
Windoze <windoze.net@gmail.com>
2014-03-25 01:35:00 +0800

    refined echo server

--- test/echo_server.cpp
+++ test/echo_server.cpp
@@ -7,22 +7,83 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <atomic>
 #include <fibio/fiber.hpp>
 #include <fibio/stream/iostream.hpp>
 
 using namespace fibio;
+std::string address("0::0");
+std::atomic<unsigned short> listen_port;
+std::atomic<bool> should_exit(false);
+std::atomic<size_t> connections(0);
+
+void hello(std::ostream &s) {
+    s << "Fiberized.IO echo_server listening at " << address << ", port " << listen_port << std::endl;
+}
+
+void help_message() {
+    std::cout << "Available commands:" << std::endl;
+    std::cout << "quit:\tquit the application" << std::endl;
+    std::cout << "info:\tshow the number of active connections" << std::endl;
+    std::cout << "help:\tshow this message" << std::endl;
+}
+
+void console() {
+    hello(std::cout);
+    help_message();
+    std::cin.tie(&std::cout);
+    while(std::cin) {
+        std::string line;
+        std::cout << "> ";
+        std::getline(std::cin, line);
+        if(line.empty()) continue;
+        if(line=="quit") {
+            break;
+        } else if (line=="info") {
+            std::cout << "Active connections: " << connections << std::endl;
+        } else if (line=="help") {
+            help_message();
+        } else {
+            std::cout << "Invalid command" << std::endl;
+            help_message();
+        }
+    }
+    should_exit=true;
+    std::cout << "Exiting..." << std::endl;
+}
 
 int main_fiber(int argc, char *argv[]) {
-    try {
-        auto acc=io::listen(atoi(argv[1]));
-        while(1) {
-            fiber([](tcp_stream s){
-                s << s.rdbuf();
-            }, io::accept(acc)).detach();
-        }
-    } catch (std::system_error &e) {
-        std::cerr << e.what() << std::endl;
+    if (argc<2) {
+        std::cerr << "Usage:" << "\t" << argv[0] << " [address] port" << std::endl;
         return 1;
+    }
+    if (argc==2) {
+        listen_port=atoi(argv[1]);
+    } else if (argc==3) {
+        address=argv[1];
+        listen_port=atoi(argv[2]);
+    }
+    fiber(console).detach();
+    auto acc=stream::tcp_acceptor(address.c_str(), listen_port, std::chrono::seconds(1));
+    while(!should_exit) {
+        std::error_code ec;
+        tcp_stream s=acc(ec);
+        if(!ec) {
+            fiber([](tcp_stream s){
+                struct conn_guard {
+                    conn_guard() {connections+=1;}
+                    ~conn_guard() {connections-=1;}
+                } guard;
+                s.set_read_timeout(std::chrono::seconds(3));
+                s.set_write_timeout(std::chrono::seconds(3));
+                hello(s);
+                s << s.rdbuf();
+            }, std::move(s)).detach();
+        } else if (ec!=std::make_error_code(std::errc::timed_out)) {
+            std::cerr << ec.message() << std::endl;
+            return ec.value();
+        }
     }
     return 0;
 }
