afcbcf12e854840a2dff473292d07c1051d8721f
Windoze <windoze.net@gmail.com>
2014-03-31 22:03:03 +0800

    delete unused code

--- include/fibio/fibers/detail/forward.hpp
+++ include/fibio/fibers/detail/forward.hpp
@@ -28,43 +28,6 @@ namespace fibio { namespace fibers { namespace detail {
     struct timed_mutex_object;
     struct timed_recursive_mutex_object;
     struct condition_variable_object;
-
-    struct fiber_async_handler {
-        fiber_async_handler();
-        void start_timer_with_cancelation(uint64_t timeout, std::function<void()> &&c);
-        void pause_current_fiber();
-        void throw_or_return(bool throw_error, boost::system::error_code &ec);
-        std::function<void(boost::system::error_code, size_t)> get_io_handler();
-        std::function<void(boost::system::error_code)> get_async_op_handler();
-        
-        std::function<void(boost::system::error_code, boost::asio::ip::tcp::resolver::iterator)> get_resolve_handler(boost::asio::ip::tcp *);
-        std::function<void(boost::system::error_code, boost::asio::ip::udp::resolver::iterator)> get_resolve_handler(boost::asio::ip::udp *);
-        std::function<void(boost::system::error_code, boost::asio::ip::icmp::resolver::iterator)> get_resolve_handler(boost::asio::ip::icmp *);
-        
-        size_t io_result() const { return io_ret_; }
-        boost::asio::ip::tcp::resolver::iterator resolve_result(boost::asio::ip::tcp *) const { return tcp_resolve_ret_; }
-        boost::asio::ip::udp::resolver::iterator resolve_result(boost::asio::ip::udp *) const { return udp_resolve_ret_; }
-        boost::asio::ip::icmp::resolver::iterator resolve_result(boost::asio::ip::icmp *) const { return icmp_resolve_ret_; }
-        
-    private:
-        void on_timeout(boost::system::error_code ec);
-        void on_async_op_complete(boost::system::error_code ec);
-        void on_io_complete(boost::system::error_code ec, size_t sz);
-        void on_tcp_resolve_complete(boost::system::error_code error, boost::asio::ip::tcp::resolver::iterator iterator);
-        void on_udp_resolve_complete(boost::system::error_code error, boost::asio::ip::udp::resolver::iterator iterator);
-        void on_icmp_resolve_complete(boost::system::error_code error, boost::asio::ip::icmp::resolver::iterator iterator);
-        
-        std::shared_ptr<detail::fiber_object> this_fiber;
-        boost::asio::basic_waitable_timer<std::chrono::steady_clock> sleep_timer;
-        uint64_t timeout_=0;
-        bool timer_triggered=false;
-        bool async_op_triggered=false;
-        std::function<void()> cancelation_;
-        size_t io_ret_;
-        boost::asio::ip::tcp::resolver::iterator tcp_resolve_ret_;
-        boost::asio::ip::udp::resolver::iterator udp_resolve_ret_;
-        boost::asio::ip::icmp::resolver::iterator icmp_resolve_ret_;
-    };
 }}} // End of namespace fibio::fibers::detail
 
 namespace fibio { namespace fibers { namespace this_fiber { namespace detail {
