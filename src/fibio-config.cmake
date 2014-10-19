e99f9748bb3f69e64004615758726095d063a3a8
Windoze <windoze.net@gmail.com>
2014-10-19 17:49:06 +0800

    Missing zlib

--- src/fibio-config.cmake
+++ src/fibio-config.cmake
@@ -6,6 +6,7 @@
 find_package(Threads REQUIRED)
 find_package(Boost 1.56 COMPONENTS system coroutine context iostreams REQUIRED)
 find_package(OpenSSL REQUIRED)
+find_package(ZLIB REQUIRED)
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/fibio-targets.cmake)
