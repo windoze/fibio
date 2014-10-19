e03f9f54df15a697fa005b29ce77d53934e8c409
Windoze <windoze.net@gmail.com>
2014-10-19 16:52:46 +0800

    Update cmake file

--- src/fibio-config.cmake
+++ src/fibio-config.cmake
@@ -4,11 +4,11 @@
 #  FIBIO_LIBRARIES
 
 find_package(Threads REQUIRED)
-find_package(Boost 1.55 COMPONENTS system coroutine context REQUIRED)
+find_package(Boost 1.56 COMPONENTS system coroutine context iostreams REQUIRED)
 find_package(OpenSSL REQUIRED)
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/fibio-targets.cmake)
 get_filename_component(FIBIO_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
-list(APPEND FIBIO_INCLUDE_DIRS ${Boost_INCLUDE_DIRS} ${OPENSSL_INCLUDE_DIRS})
-list(APPEND FIBIO_LIBRARIES fibio ${Boost_LIBRARIES} ${OPENSSL_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
+list(APPEND FIBIO_INCLUDE_DIRS ${Boost_INCLUDE_DIRS} ${OPENSSL_INCLUDE_DIRS} ${ZLIB_INCLUDE_DIRS})
+list(APPEND FIBIO_LIBRARIES fibio ${Boost_LIBRARIES} ${OPENSSL_LIBRARIES} ${ZLIB_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
